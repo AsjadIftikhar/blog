d0af56991279695ef1d4a5cdd2bec82b5f7fc833
kai <iftikharasjad@gmail.com>
2022-02-06 01:23:34 +0500

    view implemented

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from .forms import BlogForm
 
 
 def index(request):
+    idea = ""
+    graphic = ""
     if request.method == "POST":
-        pass
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            category = form.cleaned_data['category']
+            keywords = form.cleaned_data['keywords']
+
+            print(category)
+            print(keywords)
+            graphic = "none"
+            idea = "Pets are great"
     else:
-        pass
-    return render(request, 'home.html', {})
+        graphic = "block"
+        form = BlogForm()
+    return render(request, 'home.html', {'form': form, 'idea': idea, 'graphic': graphic})
