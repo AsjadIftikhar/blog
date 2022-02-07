96560e73c6d29e9b66d92f7129aeef4de1e12001
Ali Iqbal <ali.iqbal028@gmail.com>
2022-02-07 17:01:08 +0500

    Clean Output

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 # Add methods to use GPT
 from happytransformer import HappyGeneration, GENSettings
+import re
 
 
 def ideaGenerator(keyword):
@@ -42,15 +43,19 @@ def ideaGenerator(keyword):
     blog ideas for water include:
     1: Wastewater treatment: A critical component of a circular economy.
     2: A low-priced water reuse process that also delivers renewable energy in rural areas.
-    3: Time to adapt to changing climate: what does it mean for water?"
-    
+    3: Time to adapt to changing climate: what does it mean for water?
+    4: 16 Astonishing Facts About Water.
+    5: Here's how much water you should drink in a day.
+    6: Benefits of Drinking a lot of Water."
+
     blog ideas for fashion include:
     1: The wardrobe tour.
     2: The biggest fashion fails.
     3: Memorable outfits to wear on special occasions.
     4: DIY Fashion tutorials.
-    5: Your top fashion hacks."
-    
+    5: Your top fashion hacks.
+    6: Things You Should Know About The Fashion Industry."
+
     blog ideas for health care include:
     1: foods that will boost your immunity.
     2: number of exercises you can do at home with no equipment.
@@ -58,22 +63,26 @@ def ideaGenerator(keyword):
     4: Sleep quality and mental health: What the data says.
     5: Why time with friends is essential for your mental health.
     6: From Salt to Turmeric: Common kitchen foods that soothe an itchy throat."
-    
+
     blog ideas for machine learning include:
     1: Deep Learning Without Labels.
     2: Can AI Generate Programs to Help Automate Busy Work?
     3: AI-Based Virtual Tutors – The Future of Education?
     4: Transfer Learning for Text using Deep Learning Virtual Machine.
-    5: A Guide to Natural Language Processing for Text and Speech."
-    
+    5: A Guide to Natural Language Processing for Text and Speech.
+    6: Advancements in Machine Learning and Artificial Intelligence.
+    7: When and How Machine Learning can Change the World.
+    8: Do You Think You Know Everything About Machine Learning?"
+
     blog ideas for travel include:
     1: Cheapest Travel Destinations.
     2: The least-visited countries in Europe.
     3: The year to plan a trip with friends.
     4: The future of solo female travel.
-    5: The world’s most interesting journeys."
-    
-    log ideas for food include:
+    5: The world’s most interesting journeys.
+    6: Things to Keep in Mind When travelling alone."
+
+    blog ideas for food include:
     1: The Best Bagels Are in California.
     2: What Our Food Reporters and Editors Make When They’re Too Tired to Cook.
     3: 20 Simple Sauces That Will Transform Any Meal.
@@ -87,7 +96,10 @@ def ideaGenerator(keyword):
     top_k_sampling_settings = GENSettings(do_sample=True, top_k=30, max_length=15, min_length=10)
     output_top_k_sampling = gpt_neo.generate_text(prompt, args=top_k_sampling_settings)
 
-    print(output_top_k_sampling.text)
+    rawOutput = output_top_k_sampling.text
+    split = rawOutput.splitlines()
+    output = split[1]  #read second line
+    blogIdea = re.sub(r'\d+', '', output) #remove number from start
+    blogIdea = blogIdea.replace(":", "").lstrip()
 
-
-ideaGenerator("education")
+    return blogIdea
