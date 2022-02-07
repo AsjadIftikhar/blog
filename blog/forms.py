8f479f91df5ad2355f1dabe8540238a81902ba86
kai <iftikharasjad@gmail.com>
2022-02-07 15:03:09 +0500

    front end complete

--- blog/forms.py
+++ blog/forms.py
@@ -5,5 +5,3 @@ from django.forms import TextInput
 class BlogForm(forms.Form):
     category = forms.CharField(label='Category', max_length=255,
                                widget=TextInput(attrs=({'class': "form-control pill inp", "placeholder": "Category"})))
-    keywords = forms.CharField(label='Keywords', max_length=255,
-                               widget=TextInput(attrs=({'class': "form-control pill inp", "placeholder": "Keyword"})))
